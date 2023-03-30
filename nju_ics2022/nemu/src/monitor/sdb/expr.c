fbc88c82601ea29d3baef2260e698a3dc6cfed08
tracer-ics2022 <tracer@njuics.org>
2023-03-30 11:02:52 +0800

    >  compile NEMU lianhuilin@126.com billin Linux lin-pc 5.19.0-38-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 17:33:16 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:02:52 up  1:31,  1 user,  load average: 0.18, 0.14, 0.06

--- nju_ics2022/nemu/src/monitor/sdb/expr.c
+++ nju_ics2022/nemu/src/monitor/sdb/expr.c
@@ -195,7 +195,7 @@ word_t eval(int p, int q)
     else if (p == q)
     {
         word_t num;
-        
+
         if(tokens[p].type == TK_DEC) {
             sscanf(tokens[p].str, "%lu", &num);
         }
@@ -204,7 +204,10 @@ word_t eval(int p, int q)
         }
         else if(tokens[p].type == TK_REG) {
             bool success;
-            num = isa_reg_str2val(tokens[p].str, &success);
+            if(strcmp(tokens[p].str, "$0"))
+                num = isa_reg_str2val(tokens[p].str, &success);
+            else
+                num = isa_reg_str2val(tokens[p].str + 1, &success);
         }
 
         return num;
